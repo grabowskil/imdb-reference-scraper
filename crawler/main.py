2a46dea669a67d3431e628723dbcab2ea45334d1
Lennart Grabowski <lennartgrabowski@Lennarts-MacBook-Air.local>
2016-05-13 11:08:40 +0200

    fixes in import calls

--- crawler/main.py
+++ crawler/main.py
@@ -1,22 +1,22 @@
 import requests
 import time
 from bs4 import BeautifulSoup
-import acc_csv
+from . import acc_csv
 
 def imdbScraper(titleLink, wait_time=5):
     r = requests.get('http://www.imdb.com' + titleLink + '/movieconnections')
     
     start_time = time.time()
     
-    print("open new title")
+    print("process: open new title")
     if r.status_code != 404:
-        print("no 404")
+        print("request: no 404")
         soup = BeautifulSoup(r.text, 'lxml')
         
         title_tag = soup.head.title.contents
         title_str = stripTitle(str(title_tag))
         
-        print("new title = " + title_str)
+        print("process: next title '" + title_str + "'")
         
         ref_heading = soup.find('a', attrs={'name':'referenced_in'})
         
@@ -38,20 +38,20 @@ def imdbScraper(titleLink, wait_time=5):
                 if div.next_sibling.next_sibling == soup.find('a', attrs={'name':'spoofed_in'}): c = True
                 
         if acc_csv.inList(title_str) == False:
-            print("writing in csv")
+            print("process: writing in csv")
             acc_csv.writeCsv(title_str, div_list)
         else:
-            print("known title, just parsing div_list")
+            print("process: known title, just parsing div_list")
         
         elapsed_time = time.time() - start_time
         if elapsed_time < wait_time:
             sleep_time = wait_time - elapsed_time
-            print("need to wait {:.0} seconds".format(sleep_time))
+            print("process: need to wait {:.0} seconds".format(sleep_time))
             time.sleep(sleep_time)
         
         return div_list
     else:
-        print('404d :-(')
+        print('request: 404')
         return '404'
     
 def stripTitle(title):
@@ -65,13 +65,15 @@ def stripTitle(title):
         return title[titleQuotPos + 1 : titleHypPos - 1]
     
 def imdbCrawler(levelDepth=0, init_titleLink='/title/tt0133093', wait_time=5):
-    #print("level of Depth: " + str(levelDepth) + " initial title Link: " + str(init_titleLink) + " wait time: " + str(wait_time))
     if levelDepth == None: levelDepth = 0
     if init_titleLink == None: init_titleLink = '/title/tt0133093'
     if wait_time == None: wait_time = 5
+    #print("level of Depth: " + str(levelDepth) + " initial title Link: " + str(init_titleLink) + " wait time: " + str(wait_time))
+    print("process: start")
     div_list = imdbScraper(init_titleLink, wait_time)
     for _ in range(levelDepth):
         if div_list != '404':
             for div in div_list:
                 next_link = div[1]
                 imdbCrawler(levelDepth-1, next_link, wait_time)
+    print("process: done")
