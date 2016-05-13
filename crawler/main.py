4d27f08fac42bb347bebcfeb3039a986ccbdff2a
Lennart Grabowski <lennartgrabowski@Lennarts-MacBook-Air.local>
2016-05-13 18:54:31 +0200

    added counter and output to user

--- crawler/main.py
+++ crawler/main.py
@@ -6,11 +6,13 @@ def imdbCrawler(levelDepth=0, init_titleLink='/title/tt0133093', wait_time=5, al
     if wait_time == None: wait_time = 5
     if all == None: all = False
     #print("level of Depth: " + str(levelDepth) + " initial title Link: " + str(init_titleLink) + " wait time: " + str(wait_time))
-    print("crawler: start, level: " + str(levelDepth))
+    print("crawler: start on level: " + str(levelDepth))
     div_list = scraper.imdbScraper(init_titleLink, wait_time, all)
+    c = 0
     for _ in range(levelDepth):
         if div_list != '404' and div_list != None:
             for div in div_list:
                 next_link = div[1]
                 imdbCrawler(levelDepth-1, next_link, wait_time, all)
-    print("crawler: done, level: " + str(levelDepth))
+    c = len(div_list)
+    print("crawler: " + str(c) + "done on level " + str(levelDepth))
