0f7b74bc7d842005a68f3a18b4c9fc2aa651078e
Lennart Grabowski <lennartgrabowski@Lennarts-MacBook-Air.local>
2016-05-13 15:42:29 +0200

    scraper reviewed for bugs, reorganized

--- crawler/scraper.py
+++ crawler/scraper.py
@@ -6,30 +6,22 @@ from . import acc_csv
 def imdbScraper(titleLink, wait_time=5, all=False):
     if acc_csv.linkInList(titleLink) == False:
         r = requests.get('http://www.imdb.com' + titleLink + '/movieconnections')
-        
         start_time = time.time()
-        
         print("scraper: open new title", end='\r')
         if r.status_code != 404:
             print("request: new title opened")
             soup = BeautifulSoup(r.text, 'lxml')
-            
             title_tag = soup.head.title.contents
             title_str = stripTitle(str(title_tag))
             if contExcluded(title_str) == False or all == True:
                 print("scraper: next title '" + title_str + "'")
                 ref_heading = soup.find('a', attrs={'name':'referenced_in'})
-                
                 div_list = []
-                
                 if ref_heading != None:
-                
                     ref_divs = ref_heading.find_next_siblings('div')
                     ref_divsCount = len(ref_divs)
-
                     c = False
                     cntr = 0
-                    
                     for div in ref_divs:
                         cntr += 1
                         print("loading: {:.1%}".format(cntr/ref_divsCount), end='\r')
@@ -38,14 +30,12 @@ def imdbScraper(titleLink, wait_time=5, all=False):
                             div_href = div.a['href']
                             div_list.append([div_content, div_href])
                         if div.next_sibling.next_sibling == soup.find('a', attrs={'name':'spoofed_in'}): c = True
-                    
                     print("scraper: writing in csv")
                     acc_csv.writeCsv(title_str, div_list, titleLink)
-                
             elapsed_time = time.time() - start_time
             if elapsed_time < wait_time:
                 sleep_time = wait_time - elapsed_time
-                print("request: need to wait {:.0} seconds".format(sleep_time))
+                print("requests: need to wait {:.1} seconds".format(sleep_time))
                 time.sleep(sleep_time)
         else:
             print('request: 404')
