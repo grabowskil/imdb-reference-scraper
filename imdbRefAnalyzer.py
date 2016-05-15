1140c445105433108894a8f047d3ad37e38b0888
Lennart Grabowski <lennartgrabowski@Lennarts-MacBook-Air.local>
2016-05-15 12:22:33 +0200

    rudimentary analyzer implemented. imdbRefAnalyzer.py -p prints top10 from data.csv

--- imdbRefAnalyzer.py
+++ imdbRefAnalyzer.py
@@ -1,3 +1,12 @@
 import argparse
+from analyzer import main
 
 parser = argparse.ArgumentParser(description='analyze scraped references from imdb')
+parser.add_argument('-p', '--print', action='store_true', help="if parsed 'true', the top 10 movies from data.csv get printed")
+
+args = parser.parse_args()
+
+if args.print == True:
+    main.printTopTen()
+else:
+    parser.parse_args(['-h'])
