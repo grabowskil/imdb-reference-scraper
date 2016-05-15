fecff930034be6590c73e29cac38e6bfa11edda9
Lennart Grabowski <HunterXxX360@users.noreply.github.com>
2016-05-15 17:55:02 +0200

    '-r' arg implemented
    in case of urllib Error -3 the crawler will try to restart the scraper

--- imdbRefScraper.py
+++ imdbRefScraper.py
@@ -6,6 +6,7 @@ parser.add_argument('-d', '--levelDepth', type=int, help='depth, the crawler sho
 parser.add_argument('-iL', '--initialLink', type=str, help="parses the initial title-link, format: 'title/tt0000000', make sure the title exists on imdb! Default: 'The Matrix'")
 parser.add_argument('-t', '--time', type=int, help='specify amount of seconds, the scraper waits at least between every call. Default: 5')
 parser.add_argument('-a', '--all', action='store_true', help="if parsed 'true', videos, tv shows and games will be crawled. Default: false")
+parser.add_argument('-r', '--recursive', action='store_true', help="if parsed 'true', the crawler will try to restart the scraper in case of urllib Error -3 (can have unintended consequences!")
 
 args = parser.parse_args()
 
@@ -13,4 +14,5 @@ if args.levelDepth == None:
     parser.parse_args(['-h'])
 else:
     print("crawler will be launched with '-a' " + str(args.all))
-    main.imdbCrawler(args.levelDepth, args.initialLink, args.time, args.all)
+    print("crawler will be launched with '-r' " + str(args.recursive))
+    main.imdbCrawler(args.levelDepth, args.initialLink, args.time, args.all, args.recursive)
