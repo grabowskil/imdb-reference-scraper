e694af18accee916ad9073de7e9f952733af0d6d
Lennart Grabowski <lennartgrabowski@Lennarts-MacBook-Air.local>
2016-05-30 18:44:44 +0200

    prepared SQL-db

--- analyzer/visualizer.py
+++ analyzer/visualizer.py
@@ -1,3 +1,4 @@
 import matplotlib.pyplot
+import networkx
 
 pyplot.figure(figsize(12,8), dpi=100, facecolor='1.0')
--- imdbRefAnalyzer.py
+++ imdbRefAnalyzer.py
@@ -1,12 +1,16 @@
 import argparse
-from analyzer import main
+from analyzer import main as anal
+from mod import main as modi
 
 parser = argparse.ArgumentParser(description='analyze scraped references from imdb')
 parser.add_argument('-p', '--print', type=int, help="prints the defined number of movies")
+parser.add_argument('-r', '--refresh', type=store_true, help="refreshes the 'data.db' database from the 'data.csv'")
 
 args = parser.parse_args()
 
-if args.print == None:
-    parser.parse_args(['-h'])
+if args.print != None:
+    anal.printTop(args.print)
+elif args.refresh != None:
+    modi.updateDB()
 else:
-    main.printTop(args.print)
+    parser.parse_args(['-h'])
